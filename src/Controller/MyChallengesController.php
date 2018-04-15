e257f391e5fd58e2577f08d5efaa9735131f9008
Airidas Amšiejus <airidasams@gmail.com>
2018-04-16 02:26:32 +0300

    update MyChallenges Controller and Template

--- src/Controller/MyChallengesController.php
+++ src/Controller/MyChallengesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Challenges;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -12,8 +14,19 @@ class MyChallengesController extends Controller
      */
     public function index()
     {
+        $this->getChallenges();
         return $this->render('my_challenges/index.html.twig', [
             'controller_name' => 'MyChallengesController',
+            'challenges' => $this->getChallenges()
         ]);
+    }
+
+    public function getChallenges(): array
+    {
+        $challenges = $this->getDoctrine()
+            ->getRepository(Challenges::class)
+            ->findAll();
+
+        return $challenges;
     }
 }
