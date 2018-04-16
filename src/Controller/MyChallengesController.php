7c96a04b93c9d02c98557022a5721f1778a97223
Airidas Amšiejus <airidasams@gmail.com>
2018-04-16 21:57:16 +0300

    add phpDoc minor changes

--- src/Controller/MyChallengesController.php
+++ src/Controller/MyChallengesController.php
@@ -4,23 +4,27 @@ namespace App\Controller;
 
 use App\Entity\Challenges;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MyChallengesController extends Controller
 {
     /**
-     * @Route("/my/challenges", name="my_challenges")
+     * @Route("/challenges/list", name="my_challenges")
      */
-    public function index()
+    public function list(): Response
     {
-        $this->getChallenges();
         return $this->render('my_challenges/index.html.twig', [
             'controller_name' => 'MyChallengesController',
             'challenges' => $this->getChallenges()
         ]);
     }
 
+
+    /**
+     * @return array
+     */
     public function getChallenges(): array
     {
         $challenges = $this->getDoctrine()
