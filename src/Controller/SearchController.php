d7a211c04bef5c597e5a1bd1f112a829fe10c829
Airidas Amšiejus <airidasams@gmail.com>
2018-05-06 00:02:54 +0300

    search form with get method

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\SearchForm;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SearchController extends Controller
+{
+    public function searchBarAction()
+    {
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('home'))
+            ->setMethod('GET')
+            ->add('Search', SearchType::class, [
+//                FIXME remove label
+                'label' => ' '
+            ])
+            ->add('Submit', SubmitType::class)
+            ->getForm();
+
+        dump($form->createView());
+
+        return $this->render('includes/search.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
