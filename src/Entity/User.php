bad2ba836adb92686785cbcb420a7cedea19e6b2
Airidas Amšiejus <airidasams@gmail.com>
2018-05-14 09:17:47 +0300

    m:e comment

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -49,6 +49,11 @@ class User extends BaseUser
     private $notifications;
 
     /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="user", orphanRemoval=true)
+     */
+    private $comments;
+
+    /**
      * @return mixed
      */
     public function getMilestoneStatus(): Collection
@@ -88,6 +93,7 @@ class User extends BaseUser
         $this->milestoneStatus = new ArrayCollection();
         $this->ownedChallenge = new ArrayCollection();
         $this->notifications = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function addChallenge(Challenges $challenge): self
@@ -222,6 +228,37 @@ class User extends BaseUser
             // set the owning side to null (unless already changed)
             if ($notification->getUser() === $this) {
                 $notification->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getUser() === $this) {
+                $comment->setUser(null);
             }
         }
 
