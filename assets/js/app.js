f0b4c4f33fed1ac67feaac548e671b643bf1dc90
Viktoras <viktoras.rimkus@gmail.com>
2018-05-19 23:10:10 +0300

    setup list.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,14 @@
 require('bootstrap');
 const $ = require('jquery');
+const List = require('list.js');
 
+
+
+
+var monkeyList = new List('test-list', {
+    valueNames: ['js-name','js-description','js-type', 'js-dateEnd','js-dateStart'],
+    page: 2,
+    pagination: true
+
+});
 
