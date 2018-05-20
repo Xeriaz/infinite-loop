aaf898c810db7c71f6176f0645908a701690b9b3
Viktoras <viktoras.rimkus@gmail.com>
2018-05-20 20:10:30 +0300

    some fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,25 +3,26 @@ const $ = require('jquery');
 const List = require('list.js');
 
 
+$(document).ready(function() {
+    const challengeList = new List('challenge-list', {
+        valueNames: ['js-name','js-description','js-type', 'js-dateEnd','js-dateStart'],
+        page: 5,
+        pagination: true
 
+    });
+    const commentsList = new List('comments-list', {
+        valueNames: ['js-user','js-commentDate','js-commentText'],
+        page: 10,
+        pagination: true
 
-const challengeList = new List('challenge-list', {
-    valueNames: ['js-name','js-description','js-type', 'js-dateEnd','js-dateStart'],
-    page: 5,
-    pagination: true
+    });
+    const searchList = new List('search-list', {
+        valueNames: ['js-type','js-dateEnd','js-dateStart'],
+        page: 6,
+        pagination: true
+
+    });
+
 
 });
 
-const commentsList = new List('comments-list', {
-    valueNames: ['js-user','js-commentDate','js-commentText'],
-    page: 10,
-    pagination: true
-
-});
-
-const searchList = new List('search-list', {
-    valueNames: ['js-type','js-dateEnd','js-dateStart'],
-    page: 6,
-    pagination: true
-
-});
